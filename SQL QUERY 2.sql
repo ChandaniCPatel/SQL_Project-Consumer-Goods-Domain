c4572c1d5bee18d8780fb01aca06ef7157268013
Chandani Patel <130793227+ChandaniCPatel@users.noreply.github.com>
2024-10-10 11:33:17 -0400

    Delete SQL QUERY 2.sql

--- SQL QUERY 2.sql
+++
@@ -1,8 +0,0 @@
-# What is the percentage of unique product increase in 2021 vs. 2020?
-SELECT x.a AS unique_product_2020, y.b AS unique_products_2021, ROUND((B-A)*100/A, 2) AS percentage_chg
-FROM (
-      (SELECT COUNT(DISTINCT(product_code)) AS a FROM fact_sales_monthly
-      WHERE fiscal_year = 2020) x,
-      (SELECT COUNT(DISTINCT(product_code)) AS b FROM fact_sales_monthly
-      WHERE fiscal_year = 2021) y
-	 )
